3e3c93e5ec2f4e1cda9a81b08a8027243bfdeb7e
Naveenkommuri12 <143915194+Naveenkommuri12@users.noreply.github.com>
2025-05-16 12:46:43 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_OhlGsMpMR', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5l5kkms8nnr2k5163t8kq32ab8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
